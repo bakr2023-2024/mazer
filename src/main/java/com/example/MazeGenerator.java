c9dd297219201af6a5a52656263bbba2375a13d8
bakr2023-2024 <bakr2023.2024@gmail.com>
2026-02-03 01:09:26 +0200

    added Utility class and added Random object to MazeGenerator

--- src/main/java/com/example/MazeGenerator.java
+++ src/main/java/com/example/MazeGenerator.java
@@ -1,16 +1,33 @@
 package com.example;
 
-public class MazeGenerator {
-    private boolean[][] map;
-    private int width, height;
+import java.util.Random;
 
+public class MazeGenerator {
+    private final boolean[][] map;
+    private final int width, height;
+    private final Random rand;
     public MazeGenerator(int width, int height, Generators alg) {
         this.width = width;
         this.height = height;
-        map = new boolean[height][width];
+        this.map = new boolean[height][width];
+        this.rand = new Random();
+        switch (alg) {
+            case RECURSIVE_BACKTRACKER:
+                recursiveBacktracker();
+                break;
+            default:
+                break;
+        }
+    }
+
+    private void setMap() {
         for (int i = 0; i < height; i++)
             for (int j = 0; j < width; j++)
                 map[i][j] = false;
-        
+    }
+
+    private void recursiveBacktracker() {
+        setMap();
+
     }
 }
---
+++ src/main/java/com/example/Utility.java
@@ -0,0 +1,5 @@
+package com.example;
+
+public class Utility {
+    public static final int[][] DIRS = { { 0, -1 }, { 0, 1 }, { -1, 0 }, { 1, 0 } };
+}
