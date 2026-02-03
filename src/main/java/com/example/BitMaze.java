70343709b3784a4916a275f5c665e41e93248931
bakr2023-2024 <bakr2023.2024@gmail.com>
2026-02-03 23:06:59 +0200

    encapsulated map logic in BitMaze class

---
+++ src/main/java/com/example/BitMaze.java
@@ -0,0 +1,49 @@
+package com.example;
+
+import java.util.Arrays;
+
+public class BitMaze {
+    private final int[][] map;
+    private final int width, height;
+    public final static int LEFT = 0;
+    public final static int RIGHT = 2;
+    public final static int TOP = 3;
+    public final static int BOTTOM = 1;
+
+    public BitMaze(int width, int height) {
+        this.width = width;
+        this.height = height;
+        this.map = new int[height][width];
+        for (int i = 0; i < height; i++)
+            for (int j = 0; j < width; j++)
+                this.map[i][j] = 15;
+    }
+
+    public int getDir(Vertex curr, Vertex neighbor) {
+        int dx = neighbor.x - curr.x;
+        int dy = neighbor.y - curr.y;
+        if (dx == 0)
+            return dy == -1 ? 3 : 1;
+        else
+            return dx == -1 ? 0 : 2;
+    }
+
+    public boolean hasWall(Vertex curr, Vertex neighbor) {
+        int wall = getDir(curr, neighbor);
+        return (map[curr.y][curr.x] & 1 << wall) != 0;
+    }
+
+    public void setWall(Vertex curr, Vertex neighbor) {
+        int idx = getDir(curr, neighbor);
+        int oppIdx = idx < 2 ? idx + 2 : idx - 2;
+        map[curr.y][curr.x] |= 1 << idx;
+        map[neighbor.y][neighbor.x] |= 1 << oppIdx;
+    }
+
+    public void clearWall(Vertex curr, Vertex neighbor) {
+        int idx = getDir(curr, neighbor);
+        int oppIdx = idx < 2 ? idx + 2 : idx - 2;
+        map[curr.y][curr.x] &= ~(1 << idx);
+        map[neighbor.y][neighbor.x] &= ~(1 << oppIdx);
+    }
+}
---
+++ src/test/java/BitMazeTest.java
@@ -0,0 +1,63 @@
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import com.example.BitMaze;
+import com.example.Vertex;
+
+public class BitMazeTest {
+    private BitMaze maze;
+    private int width, height;
+    private Vertex curr, neighbor;
+
+    @BeforeEach
+    public void init() {
+        width = 3;
+        height = 3;
+        maze = new BitMaze(width, height);
+        curr = new Vertex(0, 0);
+        neighbor = new Vertex(1, 0);
+    }
+
+    @Test
+    public void eachCellHasAllWalls() {
+        for (int y = 0; y < height; y++) {
+            for (int x = 0; x < width; x++) {
+                assertTrue(maze.hasWall(new Vertex(x, y), new Vertex(x - 1, y)));
+                assertTrue(maze.hasWall(new Vertex(x, y), new Vertex(x + 1, y)));
+                assertTrue(maze.hasWall(new Vertex(x, y), new Vertex(x, y - 1)));
+                assertTrue(maze.hasWall(new Vertex(x, y), new Vertex(x, y + 1)));
+            }
+        }
+    }
+
+    @Test
+    public void getDirWorks() {
+        Vertex right = curr.add(1, 0);
+        Vertex left = curr.add(-1, 0);
+        Vertex top = curr.add(0, -1);
+        Vertex bottom = curr.add(0, 1);
+        assertEquals(maze.getDir(curr, right), BitMaze.RIGHT);
+        assertEquals(maze.getDir(curr, left), BitMaze.LEFT);
+        assertEquals(maze.getDir(curr, top), BitMaze.TOP);
+        assertEquals(maze.getDir(curr, bottom), BitMaze.BOTTOM);
+    }
+
+    @Test
+    public void clearWallWorks() {
+        maze.clearWall(curr, neighbor);
+        assertFalse(maze.hasWall(curr, neighbor));
+        assertFalse(maze.hasWall(neighbor, curr));
+    }
+
+    @Test
+    public void setWallWorks() {
+        maze.setWall(curr, neighbor);
+        assertTrue(maze.hasWall(curr, neighbor));
+        assertTrue(maze.hasWall(neighbor, curr));
+    }
+
+}
