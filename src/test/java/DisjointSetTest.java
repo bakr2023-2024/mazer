0a2502830de9bd70ca4d1a6cb5fdce9f260afe2e
bakr2023-2024 <bakr2023.2024@gmail.com>
2026-02-04 04:01:24 +0200

    wrote tests for DisjointSet

---
+++ src/test/java/DisjointSetTest.java
@@ -0,0 +1,31 @@
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import java.util.List;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import com.example.DisjointSet;
+import com.example.Vertex;
+
+public class DisjointSetTest {
+    private DisjointSet ds;
+    private int width, height;
+
+    @BeforeEach
+    public void init() {
+        width = 3;
+        height = 3;
+        ds = new DisjointSet(width, height);
+    }
+
+    @Test
+    public void disjointSetWorks() {
+        Vertex v1 = new Vertex(0, 0);
+        Vertex v2 = new Vertex(1, 0);
+        Vertex v3 = new Vertex(1, 1);
+        ds.union(v2, v1);
+        ds.union(v2, v3);
+        assertEquals(ds.getSize(v1), -3);
+    }
+}
