f2408320ac78611d5726367da025d17226ffa74a
bakr2023-2024 <bakr2023.2024@gmail.com>
2026-02-06 00:01:26 +0200

    tested maze solving algorithms

---
+++ src/test/java/MazeSolverTest.java
@@ -0,0 +1,36 @@
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import java.util.List;
+
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.EnumSource;
+
+import com.example.BitMaze;
+import com.example.Generators;
+import com.example.MazeGenerator;
+import com.example.MazeSolver;
+import com.example.Solvers;
+import com.example.Vertex;
+
+public class MazeSolverTest {
+    private int width = 3;
+    private int height = 3;
+    private BitMaze maze = new MazeGenerator(width, height, Generators.RECURSIVE_BACKTRACKER).getMap();
+    private Vertex start = new Vertex(0, 0);
+    private Vertex end = new Vertex(width - 1, height - 1);
+    private MazeSolver solver = new MazeSolver();
+
+    @ParameterizedTest
+    @EnumSource(Solvers.class)
+    public void testSolver(Solvers alg) {
+        List<Vertex> path = solver.solve(maze, start, end, alg);
+        assertNotNull(path, "Couldn't solve maze using " + alg.toString());
+        assertTrue(path.get(0).equals(start), "First vertex doesn't equal start vertex");
+        assertTrue(path.get(path.size() - 1).equals(end), "Last vertex doesn't equal end vertex");
+        for (int i = 0; i < path.size() - 1; i++) {
+            assertFalse(maze.hasWall(path.get(i), path.get(i + 1)), "A wall exists between the two vertices");
+        }
+    }
+}
