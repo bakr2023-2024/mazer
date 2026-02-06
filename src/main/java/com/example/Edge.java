63eaeef63d9a74f59021f378bfd623fefc6189d9
bakr2023-2024 <bakr2023.2024@gmail.com>
2026-02-06 13:34:20 +0200

    made Edge order-independent to better represent an edge in an undirected graph

--- src/main/java/com/example/Edge.java
+++ src/main/java/com/example/Edge.java
@@ -4,8 +4,23 @@ public class Edge {
     private final Vertex v1, v2;
 
     public Edge(Vertex v1, Vertex v2) {
-        this.v1 = v1;
-        this.v2 = v2;
+        if (v1.x != v2.x) {
+            if (v1.x < v2.x) {
+                this.v1 = v1;
+                this.v2 = v2;
+            } else {
+                this.v1 = v2;
+                this.v2 = v1;
+            }
+        } else {
+            if (v1.y < v2.y) {
+                this.v1 = v1;
+                this.v2 = v2;
+            } else {
+                this.v1 = v2;
+                this.v2 = v1;
+            }
+        }
     }
 
     public Vertex getV1() {
@@ -15,15 +30,13 @@ public class Edge {
     public Vertex getV2() {
         return v2;
     }
-
     @Override
     public boolean equals(Object obj) {
         if (this == obj)
             return true;
-        if (obj == null || !(obj instanceof Edge))
+        if (obj == null || !(obj instanceof Edge e))
             return false;
-        Edge oth = (Edge) obj;
-        return this.v1.equals(oth.v1) && this.v2.equals(oth.v2) || this.v1.equals(oth.v2) && this.v2.equals(oth.v1);
+        return v1.equals(e.v1) && v2.equals(v2);
     }
 
     @Override
