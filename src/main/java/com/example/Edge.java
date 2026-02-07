bbd359ba0177bf15ac96a54a7e42cd05241d5958
bakr2023-2024 <bakr2023.2024@gmail.com>
2026-02-07 04:04:14 +0200

    implemented hash code computation for Edge class

--- src/main/java/com/example/Edge.java
+++ src/main/java/com/example/Edge.java
@@ -30,13 +30,19 @@ public class Edge {
     public Vertex getV2() {
         return v2;
     }
+
+    @Override
+    public int hashCode() {
+        return 31 * v1.hashCode() + v2.hashCode();
+    }
+
     @Override
     public boolean equals(Object obj) {
         if (this == obj)
             return true;
         if (obj == null || !(obj instanceof Edge e))
             return false;
-        return v1.equals(e.v1) && v2.equals(v2);
+        return v1.equals(e.v1) && v2.equals(e.v2);
     }
 
     @Override
