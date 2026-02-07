243e123dbc9c214ccc6edfe8f7927013d44e9f6b
bakr2023-2024 <bakr2023.2024@gmail.com>
2026-02-07 16:08:47 +0200

    added the ability to render the maze solution in the GUI

--- src/main/java/com/example/MazerApp.java
+++ src/main/java/com/example/MazerApp.java
@@ -1,5 +1,7 @@
 package com.example;
 
+import java.util.List;
+
 import javafx.application.Application;
 import javafx.application.Platform;
 import javafx.geometry.Insets;
@@ -12,18 +14,31 @@ import javafx.scene.control.Button;
 import javafx.scene.control.ChoiceBox;
 import javafx.scene.control.Label;
 import javafx.scene.control.Spinner;
+import javafx.scene.control.SpinnerValueFactory;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.VBox;
 import javafx.scene.paint.Color;
 import javafx.stage.Stage;
 
 public class MazerApp extends Application {
+    private final int MAX_N = 500;
     private int width = 1280;
     private int height = 720;
     private double cellSize;
     private MazeGenerator gen = null;
+    private MazeSolver solver = new MazeSolver();
     private GraphicsContext g;
-
+    private List<Vertex> path = null;
+     private       Spinner<Integer> startXSpinner;
+       private Spinner<Integer> startYSpinner;
+       private Spinner<Integer> endXSpinner;
+      private  Spinner<Integer> endYSpinner;
+      private void setSpinners(int maxX,int maxY){
+        startXSpinner.setValueFactory(new SpinnerValueFactory.IntegerSpinnerValueFactory(0,maxX, 0));
+        startYSpinner.setValueFactory(new SpinnerValueFactory.IntegerSpinnerValueFactory(0,maxY, 0));
+        endXSpinner.setValueFactory(new SpinnerValueFactory.IntegerSpinnerValueFactory(0,maxX, maxX));
+        endYSpinner.setValueFactory(new SpinnerValueFactory.IntegerSpinnerValueFactory(0,maxY, maxY));
+      }
     private void renderMaze() {
         if (gen == null)
             return;
@@ -43,14 +58,24 @@ public class MazerApp extends Application {
                     g.strokeLine(x * cellSize, y * cellSize, x * cellSize, (y + 1) * cellSize);
             }
         }
+
+    }
+
+    private void renderSolution(List<Vertex> newPath) {
+        if (path != null) {
+            g.setFill(Color.BLACK);
+            path.forEach(v -> g.fillRect(v.x * cellSize + 1, v.y * cellSize + 1, cellSize - 1, cellSize - 1));
+        }
+        g.setFill(Color.GREEN);
+        newPath.forEach(v -> g.fillRect(v.x * cellSize + 1, v.y * cellSize + 1, cellSize - 1, cellSize - 1));
     }
     private VBox createGeneratorControls() {
         VBox vBox = new VBox();
         vBox.setSpacing(5);
         vBox.setAlignment(Pos.CENTER);
-        Spinner<Integer> widthSpinner = new Spinner<>(3, 100, 3);
+        Spinner<Integer> widthSpinner = new Spinner<>(3, MAX_N, 3);
         widthSpinner.setEditable(true);
-        Spinner<Integer> heightSpinner = new Spinner<>(3, 100, 3);
+        Spinner<Integer> heightSpinner = new Spinner<>(3, MAX_N, 3);
         heightSpinner.setEditable(true);
         ChoiceBox<String> genAlgs = new ChoiceBox<>();
         for (Generators gen : Generators.values())
@@ -60,13 +85,14 @@ public class MazerApp extends Application {
         genBtn.setOnAction(e -> {
             int mapWidth = widthSpinner.getValue();
             int mapHeight = heightSpinner.getValue();
+            setSpinners(mapWidth-1, mapHeight-1);
             Generators alg = Generators.valueOf(genAlgs.getValue());
             gen = new MazeGenerator(mapWidth, mapHeight, alg);
             cellSize = Math.min(g.getCanvas().getWidth() / mapWidth,
                             g.getCanvas().getHeight() / mapHeight);
             renderMaze();
         });
-        vBox.getChildren().addAll(new Label("Mazer"),
+        vBox.getChildren().addAll(
                 new Label("Width"), widthSpinner,
                 new Label("Height"), heightSpinner,
                 new Label("Generation Algorithms"), genAlgs, genBtn);
@@ -77,11 +103,33 @@ public class MazerApp extends Application {
         VBox vBox = new VBox();
         vBox.setSpacing(5);
         vBox.setAlignment(Pos.CENTER);
+        startXSpinner = new Spinner<>(0,MAX_N-1,0);
+        startYSpinner = new Spinner<>(0,MAX_N-1,0);
+        endXSpinner = new Spinner<>(0,MAX_N-1,MAX_N-1);
+        endYSpinner = new Spinner<>(0,MAX_N-1,MAX_N-1);
+        startXSpinner.setEditable(true);
+        startYSpinner.setEditable(true);
+        endXSpinner.setEditable(true);
+        endYSpinner.setEditable(true);
         ChoiceBox<String> solAlgs = new ChoiceBox<>();
         for (Solvers sol : Solvers.values())
             solAlgs.getItems().add(sol.toString());
+        solAlgs.setValue(Solvers.DFS.toString());
         Button solBtn = new Button("Solve");
+        solBtn.setOnAction(e -> {
+            if(gen==null)return;
+            Vertex start = new Vertex(startXSpinner.getValue(), startYSpinner.getValue());
+            Vertex end =  new Vertex(endXSpinner.getValue(), endYSpinner.getValue());
+            Solvers alg = Solvers.valueOf(solAlgs.getValue());
+            List<Vertex> solPath = solver.solve(gen.getMap(),start,end, alg);
+            renderSolution(solPath);
+            path = solPath;
+        });
         vBox.getChildren().addAll(
+                new Label("Start X"), startXSpinner,
+                                        new Label("Start Y"), startYSpinner,
+                new Label("End X"), endXSpinner,
+                new Label("End Y"), endYSpinner,
                 new Label("Solving Algorithms"), solAlgs, solBtn);
         return vBox;
     }
@@ -90,7 +138,8 @@ public class MazerApp extends Application {
     public void start(Stage stage) throws Exception {
         BorderPane mainPane = new BorderPane();
         Button simulateBtn = new Button("Simulate");
-        VBox controlsPane = new VBox(10, createGeneratorControls(), createSolverControls(), simulateBtn);
+        VBox controlsPane = new VBox(10, new Label("Mazer"), createGeneratorControls(), createSolverControls(),
+                simulateBtn);
         controlsPane.setAlignment(Pos.CENTER);
         controlsPane.setPadding(new Insets(5, 5, 5, 5));
         Canvas canvas = new Canvas(0.6 * width, height);
