c7a3667e9e5cb8acbcf2abf0d5e8dc79328ee9b2
bakr2023-2024 <bakr2023.2024@gmail.com>
2026-02-08 05:12:36 +0200

    updated the application's icon

--- src/main/java/com/example/MazerApp.java
+++ src/main/java/com/example/MazerApp.java
@@ -14,6 +14,7 @@ import javafx.scene.control.ChoiceBox;
 import javafx.scene.control.Label;
 import javafx.scene.control.Spinner;
 import javafx.scene.control.SpinnerValueFactory;
+import javafx.scene.image.Image;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.VBox;
 import javafx.scene.paint.Color;
@@ -309,6 +310,16 @@ public class MazerApp extends Application {
         mainPane.setCenter(canvas);
         mainScene.getStylesheets().add(getClass().getResource("/styles.css").toExternalForm());
         stage.setScene(mainScene);
+        stage.setOnCloseRequest(e -> {
+            MazeGenerator.stop = true;
+            MazeSolver.stop = true;
+            if (genThread != null)
+                genThread.interrupt();
+            if (solThread != null)
+                solThread.interrupt();
+            Platform.exit();
+        });
+        stage.getIcons().add(new Image(getClass().getResourceAsStream("/favicon.png")));
         stage.setTitle("Mazer");
         stage.show();
     }
